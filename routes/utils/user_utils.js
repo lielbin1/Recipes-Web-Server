983ecdaaea607b59176abb4923cecf824d615c85
hallelhel <hallelh@post.bgu.ac.il>
2022-06-27 19:35:57 +0300

    change recupe id to id

--- routes/utils/user_utils.js
+++ routes/utils/user_utils.js
@@ -41,23 +41,38 @@ async function checkIsWatched(user_id,recipe_id){
 
 
 async function getCreatedRecipes(user_id){
-    const recipes_details = await DButils.execQuery(`select title,image,readyInMinutes, aggregateLikes, vegan, vegetarian, glutenFree from recipes where user_id='${user_id}'`);
+    const recipes_details = await DButils.execQuery(`select recipe_id as id, title,image,readyInMinutes, aggregateLikes, vegan, vegetarian, glutenFree from recipes where user_id='${user_id}'`);
     return recipes_details;
 }
 
 async function getFamilyRecipes(user_id){
-    const recipes_details = await DButils.execQuery(`select title,image,readyInMinutes, aggregateLikes, vegan, vegetarian, glutenFree from recipes where user_id='${user_id}' and title like 'family%'`);
+    const recipes_details = await DButils.execQuery(`select recipe_id as id, title,image,readyInMinutes, aggregateLikes, vegan, vegetarian, glutenFree from recipes where user_id='${user_id}' and title like 'family%'`);
     return recipes_details;
 }
 
 async function getFullRecipeDetails(recipe_id){
-    const recipes_details = await DButils.execQuery(`select * from recipes where recipe_id='${recipe_id}'`);
+    const recipes_details = await DButils.execQuery(`select recipe_id as id, title,image,servings, readyInMinutes, aggregateLikes, vegan, vegetarian, glutenFree, instructions from recipes where recipe_id='${recipe_id}'`);
     const recipes_ingredients = await DButils.execQuery(`select ingredient_name,amount from recipe_ingredients where recipe_id='${recipe_id}'`);
     recipes_details[0]["extendedIngredients"] = recipes_ingredients;
     let is_favorite = await checkIsFavorite(recipe_id);
     let is_watched = await checkIsWatched(recipe_id);
     recipes_details[0]["is_favorite"]=is_favorite;
     recipes_details[0]["is_watched"]=is_watched;
+    // return { //channged
+    //     id: recipes_details.recipe_id,
+    //     title: recipes_details.title,
+    //     readyInMinutes: recipes_details.readyInMinutes,
+    //     image: recipes_details.image,
+    //     aggregateLikes: recipes_details.aggregateLikes,
+    //     vegan: recipes_details.vegan,
+    //     vegetarian: recipes_details.vegetarian,
+    //     glutenFree: recipes_details.glutenFree,
+    //     is_favorite:is_favorite,
+    //     is_watched:is_watched, 
+    //     analyzedInstructions: recipes_details.analyzedInstructions,
+    //     extendedIngredients: recipes_ingredients
+       
+    // } //end changed
 
     return recipes_details;
 }
